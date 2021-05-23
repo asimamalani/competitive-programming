c6e4a246ff4716ce66e4ba29173e647ff47c202c
Asim Amalani <asim.amalani@gmail.com>
2021-05-23 10:15:30 -0400

    88 Merge Sorted Array

---
+++ 88_Merge_Sorted_Array.py
@@ -0,0 +1,44 @@
+"""
+88. Merge Sorted Array
+Easy
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+The number of elements initialized in nums1 and nums2 are m and n respectively. You may assume that nums1 has a size equal to m + n such that it has enough space to hold additional elements from nums2.
+
+Example 1:
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+
+Example 2:
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+
+Constraints:
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[i] <= 109
+
+Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+"""
+
+class Solution:
+    # time O(m+n), space O(1)
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if n == 0:
+            return
+        i, j, k = m-1, n-1, len(nums1)-1
+        
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                k, i = k-1, i-1
+            else:
+                nums1[k] = nums2[j]
+                k, j = k-1, j-1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            k, j = k-1, j-1
